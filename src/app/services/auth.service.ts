70b050f8992ddf760ac7a8794ca2de7a54121864
William Andreu <willy14mx@gmail.com>
2023-05-10 11:44:21 +0200

    Comments have been added to the auth service for google

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,13 +11,21 @@ export class AuthService {
 
     constructor(private afAuth: AngularFireAuth) {}
 
+    /**
+     * Returns an Observable that authenticates a user with Google OAuth.
+     * @returns Observable<any>
+     */
     GoogleAuth(): Observable<any> {
+        // Create a new GoogleAuthProvider instance
         const provider = new GoogleAuthProvider();
+        // Sign in with Google OAuth using AngularFireAuth and return an Observable
         return from(this.afAuth.signInWithPopup(provider)).pipe(
-           map(async (data) => {
-               await localStorage.setItem('user', JSON.stringify(data.additionalUserInfo))
-               return data;
-           }),
+            // Map the response data and store the user data in local storage
+            map(async (data) => {
+                await localStorage.setItem('user', JSON.stringify(data.additionalUserInfo))
+                return data;
+            }),
+            // Catch any errors and log them to the console
             catchError((error) => {
                 console.log(error);
                 return error;
@@ -25,8 +33,16 @@ export class AuthService {
         );
     }
 
+    /**
+     * Returns an Observable that authenticates a user with email and password.
+     * @param email - The user's email address
+     * @param password - The user's password
+     * @returns Observable<any>
+     */
     emailLogin(email: string, password: string): Observable<any> {
+        // Sign in with email and password using AngularFireAuth and return an Observable
         return from(this.afAuth.signInWithEmailAndPassword(email, password)).pipe(
+            // Catch any errors and log them to the console
             catchError((error) => {
                 console.log(error);
                 return error;
